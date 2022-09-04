1ad689258f722e19b30e8649dc5a475f5fb9ec9e
David Tigre <tigredavid007@gmail.com>
2022-09-04 17:35:55 -0300

    Issue #1 - Retorno página HTML

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 const http = require('http');
+const { getFileContent } = require('./static-file-manager')
+const header = require('./header-response')
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
+const handleGetRequest = async (req, res) => {
+    let fileName = req.url.substring(1);
+    if(!fileName) {
+        fileName = 'page.html'
+        header.setHtmlContent(res)
+    }
+
+    const fileContent = await getFileContent(fileName)
+    header.setSuccess(res)
+    res.end(fileContent)
+}
+
+const server = http.createServer(async (req, res) => {
+    switch (req.method) {
+        case "GET":
+            await handleGetRequest(req, res)
+            break;
+        case "POST":
+            header.setCors(res)
+            res.end()
+            break;
+        case "OPTIONS":
+            header.setCors(res)
+            res.end()
+            break;
+        default:
+            header.setBadRequest(res)
+            res.end()
+            break;
+    }
 });
 
 server.listen(port, hostname, () => {
